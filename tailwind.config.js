f2c04cc6c5639cb1c8cc502ea426b4c8d5a3adcc
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2024-11-10 23:58:57 +0100

    Implement the initial structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-    extend: {},
+  experimental: {
+    optimizeUniversalDefaults: true,
   },
-  plugins: [],
+  theme: {
+    screens: {
+      mobile: { max: "520px" },
+      sm: "521px",
+      md: "912px",
+      lg: "1440px",
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+    },
+  },
 };
