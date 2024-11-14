6fa51103ba6a5f5eb92161bb0ac731498b123d5a
petemihaylov <peps.mihailov@gmail.com>
2024-11-14 12:17:27 +0000

    Add BentoSection

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./content/HeroSection";
 import ContactSection from "./content/ContactSection";
+import BentoSection from "./content/BentoSection";
 
 const LandingPage = () => (
   <>
     <HeroSection />
+    <BentoSection />
     <ContactSection />
   </>
 );
--- src/pages/landing/content/ContactSection.tsx
+++ src/pages/landing/content/ContactSection.tsx
@@ -13,9 +13,9 @@ const ContactSection = () => (
           className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
           style={{
             clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-        ></div>
+        />
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
@@ -25,10 +25,17 @@ const ContactSection = () => (
           Aute magna irure deserunt veniam aliqua magna enim voluptate.
         </p>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form
+        action="#"
+        method="POST"
+        className="mx-auto mt-16 max-w-xl sm:mt-20"
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label htmlFor="first-name" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold text-gray-900"
+            >
               First name
             </label>
             <div className="mt-2.5">
@@ -42,7 +49,10 @@ const ContactSection = () => (
             </div>
           </div>
           <div>
-            <label htmlFor="last-name" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold text-gray-900"
+            >
               Last name
             </label>
             <div className="mt-2.5">
@@ -56,7 +66,10 @@ const ContactSection = () => (
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="company" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="company"
+              className="block text-sm font-semibold text-gray-900"
+            >
               Company
             </label>
             <div className="mt-2.5">
@@ -70,7 +83,10 @@ const ContactSection = () => (
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-900"
+            >
               Email
             </label>
             <div className="mt-2.5">
@@ -84,7 +100,10 @@ const ContactSection = () => (
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="phone-number"
+              className="block text-sm font-semibold text-gray-900"
+            >
               Phone number
             </label>
             <div className="relative mt-2.5">
@@ -111,7 +130,10 @@ const ContactSection = () => (
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm font-semibold text-gray-900">
+            <label
+              htmlFor="message"
+              className="block text-sm font-semibold text-gray-900"
+            >
               Message
             </label>
             <div className="mt-2.5">
@@ -120,7 +142,7 @@ const ContactSection = () => (
                 id="message"
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-              ></textarea>
+              />
             </div>
           </div>
           <div className="flex gap-x-4 sm:col-span-2">
@@ -138,14 +160,15 @@ const ContactSection = () => (
                 <span
                   aria-hidden="true"
                   className="size-4 translate-x-0 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                ></span>
+                />
               </button>
             </div>
             <label className="text-sm text-gray-600" id="switch-1-label">
               By selecting this, you agree to our
               <a href="#" className="font-semibold ml-2 text-indigo-600">
                 privacy&nbsp;policy
-              </a>.
+              </a>
+              .
             </label>
           </div>
         </div>
