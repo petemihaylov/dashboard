3c10e38aa77a97e4509eddb10751ee155778250a
petemihaylov <peps.mihailov@gmail.com>
2024-11-17 22:36:04 +0100

    Add services cards

--- src/pages/landing/content/HeroSection.tsx
+++ src/pages/landing/content/HeroSection.tsx
@@ -1,43 +1,54 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Button } from "@headlessui/react";
+import { TextGenerateEffect } from "../../../components/TextGenerateEffect";
 
-const HeroSection = () => (
-  <motion.section
-    className="relative h-screen"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 1 }}
-  >
-    <img
-      src="assets/images/background.jpeg?height=1080&width=1920"
-      alt="Hero background"
-      className="absolute inset-0 object-cover w-full h-full"
-    />
-    <div className="absolute inset-0 bg-black/50" />
-    <div className="relative container mx-auto px-4 h-full flex items-center">
-      <motion.div
-        className="max-w-2xl text-white"
-        initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5, duration: 0.8 }}
-      >
-        <h1 className="text-5xl font-bold tracking-tighter sm:text-6xl lg:text-7xl">
-          Dive into Adventure
-        </h1>
-        <p className="mt-6 text-xl">
-          Experience the wonders of the underwater world with expert-led scuba
-          diving tours and courses.
-        </p>
-        <div className="mt-10 flex gap-4">
-          <Button className="hover:text-white text-gray-300 block w-[8rem] rounded bg-indigo-600 px-3.5 h-[2.5rem]">
-            Get Started
-          </Button>
-          <Button className="hover:text-white text-gray-300">Learn More</Button>
-        </div>
-      </motion.div>
-    </div>
-  </motion.section>
-);
+const HeroSection = () => {
+  const headline = "Dive Into Your Next Adventure";
+  const subheadline =
+    "Experience the wonders of the underwater world with expert-led scuba diving tours and courses.";
+  return (
+    <motion.section
+      className="relative h-screen"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <img
+        src="assets/images/background.jpeg?height=1080&width=1920"
+        alt="Hero background"
+        className="absolute inset-0 object-cover w-full h-full "
+      />
+      <div className="absolute inset-0 bg-[#0d0a22]/60 h-full " />
+      <div className="relative container mx-auto px-4 sm:h-full h-[90vh] flex items-center">
+        <motion.div
+          className="max-w-3xl text-white sm:px-0 px-1"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 0.8 }}
+        >
+          <h1 className="sm:text-5xl text-3xl font-bold tracking-tighter sm:text-6xl lg:text-7xl">
+            <TextGenerateEffect duration={2} filter={false} words={headline} />
+          </h1>
+          <p className="mt-6 sm:text-xl text-sm">
+            <TextGenerateEffect
+              duration={2}
+              filter={false}
+              words={subheadline}
+            />
+          </p>
+          <div className="mt-10 flex gap-4">
+            <Button className="hover:text-white text-gray-300 block w-[8rem] rounded bg-indigo-600 px-3.5 h-[2.5rem]">
+              Contact Us
+            </Button>
+            <Button className="hover:text-white text-gray-300">
+              Learn More
+            </Button>
+          </div>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
+};
 
 export default HeroSection;
