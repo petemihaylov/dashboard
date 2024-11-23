3efaf09f67da660d847c09d7decbd44e78566b05
petemihaylov <peps.mihailov@gmail.com>
2024-11-23 16:16:27 +0100

    Add carousel section

---
+++ src/components/courses/CourseCard.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faLocationDot,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
+
+export interface CourseProps {
+  id: number;
+  title: string;
+  location: string;
+  date: string;
+  price: number;
+  image: string;
+}
+
+export const CourseCard: React.FC<CourseProps> = ({
+  id,
+  title,
+  location,
+  date,
+  price,
+  image,
+}) => (
+  <motion.div
+    className="mx-auto max-w-sm h-[400px] rounded overflow-hidden group cursor-pointer bg-white shadow-xl"
+    whileHover={{ y: -5 }}
+    transition={{ duration: 0.2 }}
+  >
+    {/* Image Container */}
+    <div className="relative h-1/2 overflow-hidden">
+      <motion.div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${image})` }}
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.4 }}
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent" />
+      <div className="absolute text-2xl  bottom-4 right-4 text-white px-4 py-2 rounded font-bold">
+        € {price}
+      </div>
+    </div>
+
+    {/* Content */}
+    <div className="p-6 h-1/2 flex flex-col justify-between bg-white">
+      <div>
+        <h3 className="text-2xl font-bold text-gray-900 mb-3">
+          {title}, {location}
+        </h3>
+        <div className="space-y-2">
+          <div className="flex items-center text-gray-600">
+            <span className="text-sm">{date}</span>
+          </div>
+        </div>
+      </div>
+
+      <p className="text-sm w-full text-right">
+        Learn More
+        <FontAwesomeIcon icon={faArrowRight} className="w-3 h-3 ml-2" />
+      </p>
+    </div>
+  </motion.div>
+);
