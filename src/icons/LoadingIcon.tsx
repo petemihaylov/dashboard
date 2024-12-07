7694a4f62f42306a67161c2bcb2402670c846e82
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2024-12-07 17:31:43 +0100

    Repair warnings and issues

--- src/icons/LoadingIcon.tsx
+++ src/icons/LoadingIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import cn from "classnames";
-import { IconProps } from "../../types/icon";
+import { IconProps } from "../types/icon";
 
 const LoadingIcon = ({
   size = "24",
